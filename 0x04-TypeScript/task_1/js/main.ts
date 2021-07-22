313abaecda36067983cf39c349716b280aa34243
Valinor13 <jwcalhoun2@gmail.com>
2021-07-22 15:34:08 -0500

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,38 @@
 interface Teacher {
-    firstName: string,
-    lastName: string,
+    readonly firstName: string,
+    readonly lastName: string,
     fullTimeEmployee: boolean,
     yearsOfExperience?: number,
     location: string,
     [key: string]: any, 
 };
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+    return firstName.charAt(0).concat('. ', lastName);
+}
+
+interface Student {
+    firstName: string;
+    lastName: string;
+}
+
+class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHOmework() {
+        return 'Currently working';
+    }
+
+    displayName() {
+        return this.firstName;
+    }
+}
