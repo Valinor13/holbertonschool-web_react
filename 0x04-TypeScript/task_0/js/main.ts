624a59cb4ad385aa83b1f599ac86512895c2ec07
Valinor13 <jwcalhoun2@gmail.com>
2021-07-22 13:13:15 -0500

    adding new repository

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string,
+};
+
+const student1: Student = {
+    firstName: 'Jay',
+    lastName: 'Calhoun',
+    age: 31,
+    location: 'Tulsa',
+};
+
+const student2: Student = {
+    firstName: 'Rachel',
+    lastName: 'Lewis',
+    age: 34,
+    location: 'Tulsa',
+};
+
+const studentsList = [student1, student2];
