b1631fa05b12adb2afdc5e3ae4e34578f52d8d2b
Valinor13 <jwcalhoun2@gmail.com>
2022-03-08 14:24:45 -0600

    Fixing paragraph injection in js and src file in html

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,8 +1,8 @@
 import $ from "jquery";
 
-const paragraphs = [p1, p2, p3];
 const p1 = $(`<p>Holberton Dashboard</p>`);
 const p2 = $(`<p>Dashboard data for the students</p>`);
 const p3 = $(`<p>Copyright - Holberton School</p>`);
+const paragraphs = [p1, p2, p3];
 
-paragraphs.forEach((p) => ('body').append(p));
+paragraphs.forEach((p) => $('body').append(p));
