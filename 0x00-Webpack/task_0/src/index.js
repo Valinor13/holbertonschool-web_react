27014f09b9a3fb767cdb05c3a73233a2454d5922
Valinor13 <jwcalhoun2@gmail.com>
2022-03-08 13:42:01 -0600

    Adding webpack project

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+const $ = require('jquery');
+
+const p = $(`<p>Holberton Dashboard</p>
+             <p>Dashboard data for the students</p>
+             <p>Copyright - Holberton School</p>`);
+
+$('body').append(p);
