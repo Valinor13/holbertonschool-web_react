579df756c4ac0fe2106905e0313c43efee908a93
Valinor13 <jwcalhoun2@gmail.com>
2022-03-08 14:14:40 -0600

    Update index.js

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,8 @@
 import $ from "jquery";
 
-const p = $(`<p>Holberton Dashboard</p>
-             <p>Dashboard data for the students</p>
-             <p>Copyright - Holberton School</p>`);
+const paragraphs = [p1, p2, p3];
+const p1 = $(`<p>Holberton Dashboard</p>`);
+const p2 = $(`<p>Dashboard data for the students</p>`);
+const p3 = $(`<p>Copyright - Holberton School</p>`);
 
-$('body').append(p);
+paragraphs.forEach((p) => ('body').append(p));
