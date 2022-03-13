685e0820d407bc88161746e5f708ec05061cd085
Valinor13 <jwcalhoun2@gmail.com>
2022-03-13 12:39:17 -0500

    updating config and package

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,23 +1,13 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   mode: "development",
-  entry: { 
-    body: 'index.js'
-  },
+  entry: '../src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './public'),
   },
   devtool: 'inline-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({ title: 'Hot Module Replacement', }),
-    new CleanWebpackPlugin(),
-  ],
   optimization: {
     splitChunks: {
       chunks: 'all',
@@ -49,7 +39,7 @@ module.exports = {
     static: {
       directory: path.join(__dirname, './public'),
     },
+    hot: true,
     compress: true,
-    port: 8564,
   },
 };
