a1f3fe19228dca5d35704702c9cc005b54391422
Valinor13 <jwcalhoun2@gmail.com>
2022-03-23 15:40:46 -0500

    Update CourseListRow.js

--- 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -3,40 +3,39 @@ import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
 const styles = StyleSheet.create({
-  th: {
+  tableHeader: {
     borderBottom: '3px solid lightgrey',
   },
   colHeader: {
     textAlign: 'left',
+    borderBottom: '3px solid lightgrey',
   },
-});
-
-const headerStyle = {
-  backgroundColor: '#deb5b545'
-}
-
-const bodyStyle = {
-  backgroundColor: '#f5f5f5ab'
-}
+  bodyStyle: {
+    backgroundColor: '#f5f5f5ab'
+  },
+  headerStyle: {
+    backgroundColor: '#deb5b545'
+  }
+}); 
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
   if (isHeader) {
     if (textSecondCell) {
       return (
-        <tr style={headerStyle}>
-          <th className={css(styles.colHeader, styles.th)}>{textFirstCell}</th>
-          <th className={css(styles.colHeader, styles.th)}>{textSecondCell}</th>
+        <tr className={css(styles.headerStyle)}>
+          <th className={css(styles.colHeader)}>{textFirstCell}</th>
+          <th className={css(styles.colHeader)}>{textSecondCell}</th>
         </tr>
       )
     }
     return (
-      <tr style={headerStyle}>
-        <th className={css(styles.th)} colSpan={2}>{textFirstCell}</th>
+      <tr className={css(styles.headerStyle)}>
+        <th className={css(styles.tableHeader)} colSpan={2}>{textFirstCell}</th>
       </tr>
     )
   }
   return (
-    <tr style={bodyStyle}>
+    <tr className={css(styles.bodyStyle)}>
       <td>{textFirstCell}</td>
       <td>{textSecondCell}</td>
     </tr>
