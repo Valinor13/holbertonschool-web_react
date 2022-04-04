cb1ab864060518ee38b38b2a14373bb3bf19357d
Valinor13 <jwcalhoun2@gmail.com>
2022-04-04 15:33:53 -0500

    Update 8-seq.js

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -1,4 +1,4 @@
-import { seq } from "immutable";
+import { Seq } from 'immutable';
 
 // printBestStudents - accepts an object with a key of score, and number value...
 // Filters out scores < 70..
@@ -6,8 +6,8 @@ import { seq } from "immutable";
 // @object: object with key of score, and number value
 // @return: none
 export default function printBestStudents(object) {
-  const students = seq(object).filter(student => student.get('score') >= 70);
-  students.forEach(student => {
+  const students = Seq(object).filter((student) => student.get('score') >= 70);
+  students.forEach((student) => {
     console.log(`${student.get('firstName').toUpperCase()} ${student.get('lastName').toUpperCase()}`);
   });
 }
