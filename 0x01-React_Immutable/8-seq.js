4121f17d6ef74ce948059740802f7417e312902b
Valinor13 <jwcalhoun2@gmail.com>
2022-04-04 16:23:27 -0500

    Update 8-seq.js

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -6,13 +6,17 @@ import { Seq } from 'immutable';
 // @object: object with key of score, and number value
 // @return: none
 export default function printBestStudents(object) {
-  for (let key in object) {
+  const students = {};
+  for (const key in object) {
     if (object[key].score >= 70) {
-      object[key].firstName = object[key].firstName.charAt(0).toUpperCase() + object[key].firstName.slice(1);
-      object[key].lastName = object[key].lastName.charAt(0).toUpperCase() + object[key].lastName.slice(1);
-    } else {
-      delete object[key];
+      students[key].firstName =
+        object[key].firstName.charAt(0).toUpperCase() +
+        object[key].firstName.slice(1);
+      students[key].lastName =
+        object[key].lastName.charAt(0).toUpperCase() +
+        object[key].lastName.slice(1);
+      students[key].score = object[key].score;
     }
   }
-  console.log(Seq(object).toJS());
+  console.log(Seq(students).toJS());
 }
