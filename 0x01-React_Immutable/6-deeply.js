6afccc045514b72f487134cc512ff25226369339
Valinor13 <jwcalhoun2@gmail.com>
2022-04-04 14:32:08 -0500

    Create 6-deeply.js

---
+++ 0x01-React_Immutable/6-deeply.js
@@ -0,0 +1,9 @@
+import { Map } from 'immutable';
+
+// mergeDeeplyElements - accepts two objects and returns the deeply merged Map
+// @page1: First object to merge
+// @page2: Second object to merge
+// @return: deeply merged Map
+export function mergeDeeplyElements(page1, page2) {
+  return Map(page1).mergeDeep(page2);
+}
