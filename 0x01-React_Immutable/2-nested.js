ec20e4b9bfe04cf4cce25af698a0569f825a5e9b
Valinor13 <jwcalhoun2@gmail.com>
2022-04-04 13:46:38 -0500

    Update 2-nested.js

--- 0x01-React_Immutable/2-nested.js
+++ 0x01-React_Immutable/2-nested.js
@@ -1,6 +1,6 @@
-import { Map } from 'immutable';
+import { getIn } from 'immutable';
 
 // access object and return value at path definded by array
 export default function accessImmutableObject(object, array) {
-  return Map(object).getIn(array, undefined);
+  return getIn(object, array);
 }
