9c849a817f4ba99674ba3e5f274c4c3d04c2cb13
Valinor13 <jwcalhoun2@gmail.com>
2022-04-07 21:22:24 -0500

    Adding task 7

---
+++ 0x02-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/notificationActionTypes.js
@@ -0,0 +1,9 @@
+export const NotificationActionType = {
+  MARK_AS_READ: 'MARK_AS_READ',
+  SET_TYPE_FILTER: 'SET_TYPE_FILTER',
+}
+
+export const NotificationTypeFilters = {
+  DEFAULT: 'DEFAULT',
+  URGENT: 'URGENT',
+}
