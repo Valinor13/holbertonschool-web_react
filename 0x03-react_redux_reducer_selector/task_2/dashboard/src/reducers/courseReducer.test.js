0a9ede4cd3b0d8d8f70cc44f364d95cdcd15afb9
Jay Calhoun <68342564+Valinor13@users.noreply.github.com>
2022-04-12 13:53:01 -0500

    Adding tests for task 2

---
+++ 0x03-react_redux_reducer_selector/task_2/dashboard/src/reducers/courseReducer.test.js
@@ -0,0 +1,85 @@
+import { courseReducer } from "./courseReducer";
+
+describe("Test suite for courseReducer", () => {
+  it("test that the default state returns an empty array", () => {
+    const state = [];
+    expect(courseReducer(undefined, {})).toEqual(state);
+  });
+
+  it(`test that FETCH_COURSE_SUCCESS returns the data passed`, () => {
+    const data = [
+      {
+        id: 1,
+        name: "ES6",
+        isSelected: false,
+        credit: 60,
+      },
+      {
+        id: 2,
+        name: "Webpack",
+        isSelected: false,
+        credit: 20,
+      },
+      {
+        id: 3,
+        name: "React",
+        isSelected: false,
+        credit: 40,
+      },
+    ];
+    const action = { type: "FETCH_COURSE_SUCCESS", data };
+    expect(courseReducer(undefined, action)).toEqual(data);
+  });
+
+  it(`test that SELECT_COURSE returns the data with the right item updated`, () => {
+    const index = 2;
+    const data = [
+      {
+        id: 1,
+        name: "ES6",
+        isSelected: false,
+        credit: 60,
+      },
+      {
+        id: 2,
+        name: "Webpack",
+        isSelected: true,
+        credit: 20,
+      },
+      {
+        id: 3,
+        name: "React",
+        isSelected: false,
+        credit: 40,
+      },
+    ];
+    const action = { type: "SELECT_COURSE", index };
+    expect(courseReducer(data, action)).toEqual(data);
+  });
+
+  it(`test that UNSELECT_COURSE returns the data with the right item updated`, () => {
+    const index = 2;
+    const data = [
+      {
+        id: 1,
+        name: "ES6",
+        isSelected: false,
+        credit: 60,
+      },
+      {
+        id: 2,
+        name: "Webpack",
+        isSelected: false,
+        credit: 20,
+      },
+      {
+        id: 3,
+        name: "React",
+        isSelected: false,
+        credit: 40,
+      },
+    ];
+    const action = { type: "UNSELECT_COURSE", index };
+    expect(courseReducer(data, action)).toEqual(data);
+  });
+});
